f178eb1cbf58b8228fa28590ef45dcadfbca3837
Tushar Maithani <202051194@iiitvadodara.ac.in>
2024-03-18 17:52:01 +0530

    Left Info Container added

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LeftIntoContainer from '../components/LeftIntoContainer'
+
+const page = () => {
+  return (
+    <div className=''>
+        <LeftIntoContainer />
+    </div>
+  )
+}
+
+export default page
---
+++ app/components/LeftIntoContainer.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const LeftIntoContainer = () => {
+  return (
+    <div className="w-1/3 h-screen flex flex-col justify-center items-center">
+      <div>
+        <span className="font-extrabold text-5xl">Strings</span>
+        <div className="text-2xl">
+          A social Media{" "}
+          <span className="font-bold text-green-500">Application</span>
+        </div>
+        <div className="text-xl">
+          <span className="text-green-500 font-semibold">Best</span> Alternative
+          for X
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LeftIntoContainer;
