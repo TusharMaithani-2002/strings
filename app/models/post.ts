fd1d8c6fe3ecabc3dbde3e4e65f4db5750bdb097
sarthak18t <sarthaktailor@gmail.com>
2024-04-07 16:02:30 +0530

    post card changes made

--- app/models/post.ts
+++ app/models/post.ts
@@ -1,38 +1,37 @@
+
 import {Schema,model,models} from "mongoose";
 
-
 const postSchema = new Schema({
-    images: [{type:String}],
+    images: [{ type: String }],
     likesCount: {
-        type:Number,
-        default:0
+        type: Number,
+        default: 0
     },
-    repliesCount:{
-        type:Number,
-        default:0
+    repliesCount: {
+        type: Number,
+        default: 0
     },
-    replies:[{
-        type:Schema.Types.ObjectId,
-        ref:'String'
+    replies: [{
+        type: Schema.Types.ObjectId,
+        ref: 'String'
     }],
-    author:{
-        type:Schema.Types.ObjectId,
-        ref:'User',
-        required:true
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
     },
-    content:String,
-    mentions:[
-        {
-            type:Schema.Types.ObjectId,
-            ref:'User'
-        }
-    ],
-    group:{
-        type:Schema.Types.ObjectId,
-        ref:'Group'
+    content: String,
+    mentions: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    group: {
+        type: Schema.Types.ObjectId,
+        ref: 'Group'
     },
-    tags:[{type:String}]
-});
+    tags: [{ type: String }],
+},{timestamps:true});
+
 
 const Post = models.Post || model("Post",postSchema);
 
