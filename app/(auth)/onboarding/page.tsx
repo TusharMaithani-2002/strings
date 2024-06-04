5ec899107faa50965f64a4e9a94abb9fa944c475
Tushar Maithani <202051194@iiitvadodara.ac.in>
2024-06-04 17:12:41 +0530

    fixed onboarding page redirect

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { getUser } from '@/actions/user.action';
 import AccountProfile from '@/app/components/Form/AccountProfile';
+import { useAppContext } from '@/app/context/context';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 import React, { useEffect, useState } from 'react'
@@ -10,16 +11,11 @@ const Page = () => {
 
   const {data:session,status} = useSession();
   const router = useRouter();
-  const [user,setUser] = useState();
+  // @ts-ignore
+  const {user} = useAppContext();
   console.log(session,status)
 
-  useEffect(()=>{
-    const fetchUser = async() => {
-      const user = await getUser(session?.user?.id);
-      setUser(user);
-    }
-    fetchUser();
-  },[session?.user?.id])
+
   if(status === 'loading') return <div>Loading...</div>
 
   if(!session) router.push('/')
